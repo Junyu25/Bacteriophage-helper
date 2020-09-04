e11f5d2c8cfd362c45d825fa1f12088a16c2fb8c
Junyu Chen <chenjunyu2016@email.szu.edu.cn>
2020-09-04 17:31:54 +0800

    Update Assembly_quick.py

--- Scripts/Assembly_quick.py
+++ Scripts/Assembly_quick.py
@@ -31,16 +31,16 @@ def RunSpadesDirectory(inputDir, ouputDir):
                 #print(R1)
                 #print(R2)
                 R2List.append(R2)
-            sampleStr = os.path.splitext(file)[0]
-            outputFilePath = os.path.join(ouputDir, sampleStr)
-            outFileList.append(outputFilePath)
-            
-            SpadesOutDir = os.path.join(ouputDir, sampleStr, "assemble")
-            SpadesFilePath = os.path.join(SpadesOutDir, "scaffolds.fasta")
-            
-            SpadesOutList.append(SpadesOutDir)
-            SpadesFileList.append(SpadesFilePath)
-            BandageOutList.append(os.path.join(ouputDir, sampleStr, "preview.png"))
+                sampleStr = os.path.splitext(file)[0]
+                outputFilePath = os.path.join(ouputDir, sampleStr)
+                outFileList.append(outputFilePath)
+                
+                SpadesOutDir = os.path.join(ouputDir, sampleStr, "assemble")
+                SpadesFilePath = os.path.join(SpadesOutDir, "scaffolds.fasta")
+                
+                SpadesOutList.append(SpadesOutDir)
+                SpadesFileList.append(SpadesFilePath)
+                BandageOutList.append(os.path.join(ouputDir, sampleStr, "preview.png"))
     #make out dir for every run
     os.makedirs(os.path.join(ouputDir, sampleStr), 0o777, True)
 
